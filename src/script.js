d3f52ee937c8c83d0c78b9e094de0fdd61a1956d
a2c <atsushi1230.sdi@gmail.com>
2022-02-12 13:22:42 +0900

    fillRect

---
+++ src/script.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "blue";
+ctx.fillRect(10, 10, 100, 100);
+ctx.fillStyle = "red";
+ctx.fillRect(200, 10, 100, 100);
