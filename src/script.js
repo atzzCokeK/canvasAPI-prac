f55cdf9081848c99aedc2869526d2ce52415b983
a2c <atsushi1230.sdi@gmail.com>
2022-02-12 13:27:10 +0900

    add strokeRect

--- src/script.js
+++ src/script.js
@@ -1,7 +1,13 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+// fillRect
 ctx.fillStyle = "blue";
 ctx.fillRect(10, 10, 100, 100);
 ctx.fillStyle = "red";
 ctx.fillRect(200, 10, 100, 100);
+
+// strokeRect
+ctx.lineWidth = 5;
+ctx.strokeStyle = "green";
+ctx.strokeRect(10, 200, 100, 100);
