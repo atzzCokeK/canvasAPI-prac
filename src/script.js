44b22d7e978174035b816c397df8a9ead81e46ee
a2c <atsushi1230.sdi@gmail.com>
2022-02-14 01:26:00 +0900

    add animation2

--- src/script.js
+++ src/script.js
@@ -50,43 +50,155 @@ const ctx = canvas.getContext("2d");
 // ctx.fill();
 
 // Animation-1
-const circleProps = {
-  x: 200,
+// const circleProps = {
+//   x: 200,
+//   y: 200,
+//   size: 30,
+//   xd: 5,
+//   yd: 4,
+// };
+
+// function drawCircle() {
+//   ctx.beginPath();
+//   ctx.arc(circleProps.x, circleProps.y, circleProps.size, 0, Math.PI * 2);
+//   ctx.fillStyle = "red";
+//   ctx.fill();
+// }
+
+// function update() {
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   drawCircle();
+
+//   circleProps.x += circleProps.xd;
+//   circleProps.y += circleProps.yd;
+
+//   if (
+//     circleProps.x + circleProps.size > canvas.width ||
+//     circleProps.x - circleProps.size < 0
+//   ) {
+//     circleProps.xd *= -1;
+//   }
+
+//   if (
+//     circleProps.y + circleProps.size > canvas.height ||
+//     circleProps.y - circleProps.size < 0
+//   ) {
+//     circleProps.yd *= -1;
+//   }
+
+//   requestAnimationFrame(update);
+// }
+
+// update();
+
+// Animation2
+
+const image = document.getElementById("gengar");
+
+const player = {
+  w: 150,
+  h: 150,
+  x: 20,
   y: 200,
-  size: 30,
-  xd: 5,
-  yd: 4,
+  speed: 10,
+  dx: 0,
+  dy: 0,
 };
 
-function drawCircle() {
-  ctx.beginPath();
-  ctx.arc(circleProps.x, circleProps.y, circleProps.size, 0, Math.PI * 2);
-  ctx.fillStyle = "red";
-  ctx.fill();
+function drawPlayer() {
+  ctx.drawImage(image, player.x, player.y, player.w, player.h);
+}
+
+// clear canvas
+function clear() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+function detectWalls() {
+  // left wall
+  if (player.x < 0) {
+    player.x = 0;
+  }
+
+  // right wall
+  if (player.x + player.w > canvas.width) {
+    player.x = canvas.width - player.w;
+  }
+
+  // top wall
+  if (player.y < 0) {
+    player.y = 0;
+  }
+
+  // bottom wall
+  if (player.y + player.h > canvas.height) {
+    player.y = canvas.height - player.h;
+  }
+}
+
+function newPos() {
+  player.x += player.dx;
+  player.y += player.dy;
+}
+
+function moveUp() {
+  player.dy = -player.speed;
+}
+
+function moveDown() {
+  player.dy = player.speed;
+}
+
+function moveRight() {
+  player.dx = player.speed;
+}
+
+function moveLeft() {
+  player.dx = -player.speed;
+}
+
+function keyDown(e) {
+  if (e.key === "ArrowRight") {
+    moveRight();
+  }
+
+  if (e.key === "ArrowLeft") {
+    moveLeft();
+  }
+
+  if (e.key === "ArrowUp") {
+    moveUp();
+  }
+
+  if (e.key === "ArrowDown") {
+    moveDown();
+  }
+}
+
+function keyUp(e) {
+  if (
+    e.key === "ArrowRight" ||
+    e.key === "ArrowLeft" ||
+    e.key === "ArrowUp" ||
+    e.key === "ArrowDown"
+  ) {
+    player.dx = 0;
+    player.dy = 0;
+  }
 }
 
 function update() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawCircle();
+  clear();
 
-  circleProps.x += circleProps.xd;
-  circleProps.y += circleProps.yd;
+  newPos();
 
-  if (
-    circleProps.x + circleProps.size > canvas.width ||
-    circleProps.x - circleProps.size < 0
-  ) {
-    circleProps.xd *= -1;
-  }
+  detectWalls();
 
-  if (
-    circleProps.y + circleProps.size > canvas.height ||
-    circleProps.y - circleProps.size < 0
-  ) {
-    circleProps.yd *= -1;
-  }
+  drawPlayer();
 
   requestAnimationFrame(update);
 }
 
 update();
+
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
