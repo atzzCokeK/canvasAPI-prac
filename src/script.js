54c96aa4e2ce4da6a4af30825231b005e6ea4198
a2c <atsushi1230.sdi@gmail.com>
2022-02-13 14:29:38 +0900

    comment out

--- src/script.js
+++ src/script.js
@@ -1,19 +1,29 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-// fillRect
-ctx.fillStyle = "blue";
-ctx.fillRect(20, 20, 150, 100);
-ctx.fillStyle = "red";
-ctx.fillRect(200, 10, 100, 100);
+// // fillRect
+// ctx.fillStyle = "blue";
+// ctx.fillRect(20, 20, 150, 100);
+// ctx.fillStyle = "red";
+// ctx.fillRect(200, 10, 100, 100);
 
-// strokeRect
-ctx.lineWidth = 5;
-ctx.strokeStyle = "green";
-ctx.strokeRect(10, 200, 100, 100);
+// // strokeRect
+// ctx.lineWidth = 5;
+// ctx.strokeStyle = "green";
+// ctx.strokeRect(10, 200, 100, 100);
 
-// clearRect
-ctx.clearRect(25, 25, 140, 90);
+// // clearRect
+// ctx.clearRect(25, 25, 140, 90);
+
+// // fillText
+// ctx.font = "30px Arial";
+// ctx.fillStyle = "purple";
+// ctx.fillText("Hello World", 400, 50);
+
+// // strokeText
+// ctx.lineWidth = 1;
+// ctx.strokeStyle = "yellow";
+// ctx.strokeText("Hello World", 400, 200);
 
 // fillText
 ctx.font = "30px Arial";
